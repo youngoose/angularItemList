19d7598c5672ff500bee582ce69af853ac55f3d2
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2018-02-02 15:50:05 -0500

    deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDEbgxIcAbTdNfV5tSUWh9vzB_rYUBfxro",
+    authDomain: "fs1prod-a9460.firebaseapp.com",
+    databaseURL: "https://fs1prod-a9460.firebaseio.com",
+    projectId: "fs1prod-a9460",
+    storageBucket: "fs1prod-a9460.appspot.com",
+    messagingSenderId: "1014049064090"
+  }
 };
