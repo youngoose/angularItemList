f52081809f58f3c98d0a4045b182a2fd5383e63c
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2018-02-02 15:18:32 -0500

    initial commit itemlist v1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDEbgxIcAbTdNfV5tSUWh9vzB_rYUBfxro",
+    authDomain: "fs1prod-a9460.firebaseapp.com",
+    databaseURL: "https://fs1prod-a9460.firebaseio.com",
+    projectId: "fs1prod-a9460",
+    storageBucket: "fs1prod-a9460.appspot.com",
+    messagingSenderId: "1014049064090"
+  }
 };
